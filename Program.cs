297bd49a2777ea61c29676eb816b18a381975acb
SSPENELOPE <typoepping@gmail.com>
2023-02-02 13:31:39 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CatWorx.BadgeMaker {
+  class Program {
+    static void Main(string[] args) {
+        /* Console.log */
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
