20eaae70a24c26f5727bcb43837ae2aa15916905
SSPENELOPE <typoepping@gmail.com>
2023-02-03 12:37:50 -0500

    changed notes to a md file, created new functions in employee.cs

--- Program.cs
+++ Program.cs
@@ -1,35 +1,51 @@
 ﻿using System.Collections.Generic;
 
-namespace CatWorx.BadgeMaker {
-    class Program {
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
         /* Function to get employees names */
-        static List<Employee> GetEmployees() {
+        static List<Employee> GetEmployees()
+        {
             List<Employee> employees = new List<Employee>();
-            while (true) {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine() ?? "";
-                if (input == "") {
+            while (true)
+            {
+                Console.WriteLine("Please enter first name: (leave empty to exit): ");
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
+                {
                     break;
                 }
-                Employee currentEmployee = new Employee(input, "smith");
+
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
                 employees.Add(currentEmployee);
             }
-                // This is important!
-                // Passes the stored employees to the main
-                return employees;
+            // This is important!
+            // Passes the stored employees to the main
+            return employees;
         }
         /* Function to print employee names */
-        static void PrintEmployees(List<Employee> employees) {
-            for (int i = 0; i < employees.Count; i++) {
-                Console.WriteLine(employees[i].GetFullName());
+        static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
             }
         }
 
         /* Main function/entry point*/
-        static void Main(string[] args) {
-              // This is our employee-getting code now
-                List<Employee> employees = GetEmployees();
-                PrintEmployees(employees);
+        static void Main(string[] args)
+        {
+            // This is our employee-getting code now
+            List<Employee> employees = GetEmployees();
+            PrintEmployees(employees);
         }
     }
 }
