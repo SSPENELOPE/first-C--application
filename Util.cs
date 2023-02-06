fd96ba06ba0c30d208983962f456efdcc81ab3c3
SSPENELOPE <typoepping@gmail.com>
2023-02-06 10:54:52 -0500

    added method to print company name, added comments to Util.cs

--- Util.cs
+++ Util.cs
@@ -8,10 +8,8 @@ namespace CatWorx.BadgeMaker
 {
     class Util
     {
-
-
         // Method declared as "static"
-        /* Function to print employee names */
+        // Function to print employee names 
         public static void PrintEmployees(List<Employee> employees)
         {
             for (int i = 0; i < employees.Count; i++)
@@ -21,28 +19,32 @@ namespace CatWorx.BadgeMaker
             }
         }
 
+        // Function to save employee data to a .csv file
         public static void MakeCSV(List<Employee> employees)
         {
             // Check to see if folder exists
             if (!Directory.Exists("data"))
             {
-                // If not, create it
+                // If no, create it
                 Directory.CreateDirectory("data");
             }
-
+            // using Streamwriter will clear the memory after funcion has executed
             using (StreamWriter file = new StreamWriter("data/employees.csv"))
             {
+                // Immediatley write this to the top of the .csv file
                 file.WriteLine("ID,Name,PhotoUrl");
                 // Loop over employees
                 for (int i = 0; i < employees.Count; i++)
                 {
-                    // Write each employee to the file
+                    // create the template in which the items will be written to the file
                     string template = "{0},{1},{2}";
+                    // Write the items to the file using the template
                     file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
                 }
             }
         }
 
+        // Function to make employee badge
         async public static Task MakeBadges(List<Employee> employees)
         {
             // instance of HttpClient is disposed after code in the block has run
@@ -57,31 +59,28 @@ namespace CatWorx.BadgeMaker
                 int COMPANY_NAME_Y = 150;
                 for (int i = 0; i < employees.Count; i++)
                 {
-                    /* Get the employee photo */
+                    // Get the employee photo 
                     SKImage photo = SKImage.FromEncodedData(await client.GetStreamAsync(employees[i].GetPhotoUrl()));
 
-                    /* Get the bacground image of the badge */
+                    // Get the bacground image of the badge 
                     SKImage background = SKImage.FromEncodedData(File.OpenRead("badge.png"));
 
-                    /* Create bitmap to determine the size of the badge */
+                    // Create bitmap to determine the size of the badge 
                     SKBitmap badge = new SKBitmap(BADGE_WIDTH, BADGE_HEIGHT);
 
-                    /* Turn the badge into a canvas */
+                    // Turn the badge into a canvas 
                     SKCanvas canvas = new SKCanvas(badge);
 
-                    /* Add the background and employee photo to the badge */
+                    // Add the background and employee photo to the badge 
                     canvas.DrawImage(background, new SKRect(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
                     canvas.DrawImage(photo, new SKRect(PHOTO_LEFT_X, PHOTO_TOP_Y, PHOTO_RIGHT_X, PHOTO_BOTTOM_Y));
 
-                    /* Save the image to the data folder */
+                    // Save the image to the data folder
                     SKImage finalImage = SKImage.FromBitmap(badge);
                     SKData data = finalImage.Encode();
                     data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
 
-                    /* Get the company name from Employee */
-                    employees[i].GetCompanyName();
-
-                    /* SKPAINT object to layout the what our company name will look like */
+                    // SKPAINT object to layout the what our company name will look like
                     SKPaint paint = new SKPaint();
                     paint.TextSize = 42.0f;
                     paint.IsAntialias = true;
@@ -89,6 +88,9 @@ namespace CatWorx.BadgeMaker
                     paint.IsStroke = false;
                     paint.TextAlign = SKTextAlign.Center;
                     paint.Typeface = SKTypeface.FromFamilyName("Arial");
+
+                    // Print the company name to the canvas
+                    canvas.DrawText(employees[i].GetCompanyName(), BADGE_WIDTH / 2f, COMPANY_NAME_Y, paint);
                 }
             }
         }
