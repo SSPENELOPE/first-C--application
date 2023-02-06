ac7987835ae15d9c1647c7037c03c1f72c5bb7e4
SSPENELOPE <typoepping@gmail.com>
2023-02-06 17:29:26 -0500

    added comments to PeopleFetcher.cs

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -7,17 +7,24 @@ namespace CatWorx.BadgeMaker
 {
     class PeopleFetcher
     {
+        // Api call function to get random employee data
         async public static Task<List<Employee>> GetFromApi()
         {
+            // Declare the employees variable as a new List 
             List<Employee> employees = new List<Employee>();
 
+            // Use an instance of HttpClient
             using (HttpClient client = new HttpClient())
             {
+                // This is the api call stored in a string
                 string response = await client.GetStringAsync("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+                // Parse the Json object into something readable
                 JObject json = JObject.Parse(response);
+
+                // Loop through the data 
                 foreach (JToken token in json.SelectToken("results")!)
                 {
-                    // Parse JSON data
+                    // Parse JSON data, remember this is the format used in the Employee.cs when we declared the Employee arguments
                     Employee emp = new Employee
                     (
                       token.SelectToken("name.first")!.ToString(),
@@ -25,10 +32,11 @@ namespace CatWorx.BadgeMaker
                       Int32.Parse(token.SelectToken("id.value")!.ToString().Replace("-", "")),
                       token.SelectToken("picture.large")!.ToString()
                     );
-                    
+                    // Add the employee data to the employees list
                     employees.Add(emp);
                 }
             }
+            // Remember to return the data
             return employees;
         }
     }
